5de788eae5ed1c01e98b513fc90689610b14c8a8
Stefano Verna <s.verna@datocms.com>
2019-12-02 15:50:26 +0100

    Adding example

---
+++ example/using-typescript/README.md
@@ -0,0 +1,1 @@
+# TypeScript usage example
---
+++ example/using-typescript/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Image } from "../../../dist";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Image />
+    </div>
+  );
+};
+
+export default App;

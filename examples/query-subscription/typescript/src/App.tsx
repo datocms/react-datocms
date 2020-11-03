34a6a7d85af4d20bcc905f9241847714aba94cad
Stefano Verna <s.verna@datocms.com>
2020-11-03 15:18:57 +0100

    Demo

--- examples/query-subscription/typescript/src/App.tsx
+++ examples/query-subscription/typescript/src/App.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Helmet } from "react-helmet";
-import {
-  Image,
-  renderMetaTags,
-  renderMetaTagsToString,
-  useQuerySubscription,
-} from "react-datocms";
+import { Image, renderMetaTags, useQuerySubscription } from "react-datocms";
 import { query, QueryResponseType, QueryVariables } from "./query";
 
 const App: React.FC = () => {
@@ -20,54 +15,65 @@ const App: React.FC = () => {
     enabled: true,
   });
 
-  if (!data) return <div>Loading...</div>;
-  if (error) return <div>Something bad happened</div>;
+  const metaTags = data ? data.page.seo.concat(data.site.favicon) : [];
 
-  const metaTags = data.page.seo.concat(data.site.favicon);
+  const statusMessage = {
+    connecting: "Connecting to DatoCMS...",
+    connected: "Connected to DatoCMS, receiving live updates!",
+    closed: "Connection closed",
+  };
 
   return (
     <div>
       <Helmet>{renderMetaTags(metaTags)}</Helmet>
-      <pre className="seo-inspect">
-        Look at all these juicy meta tags! ↴
-        <br />
-        <br />
-        {renderMetaTagsToString(metaTags)}
-      </pre>
       <div className="app">
         <div className="app-title">DatoCMS Blog</div>
         <div className="app-subtitle">
           News, tips, highlights, and other updates from the team at DatoCMS.
         </div>
-        {data.blogPosts.map((blogPost) => (
-          <article key={blogPost.id} className="blogPost">
-            <Image
-              className="blogPost-image"
-              fadeInDuration={1000}
-              data={blogPost.coverImage.responsiveImage}
-            />
-            <h6 className="blogPost-title">
-              <a
-                href={`https://www.datocms.com/blog/${blogPost.slug}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {blogPost.title}
-              </a>
-            </h6>
-            <div
-              className="blogPost-excerpt"
-              dangerouslySetInnerHTML={{ __html: blogPost.excerpt }}
-            />
-            <footer className="blogPost-author">
+        <div className="status">
+          {status === "connected" && <div className="connected-badge" />}
+          {statusMessage[status]}
+        </div>
+        {error && (
+          <div>
+            <h1>Error: {error.code}</h1>
+            <div>{error.message}</div>
+            {error.response && (
+              <pre>{JSON.stringify(error.response, null, 2)}</pre>
+            )}
+          </div>
+        )}
+        {data &&
+          data.blogPosts.map((blogPost) => (
+            <article key={blogPost.id} className="blogPost">
               <Image
-                className="blogPost-author-image"
-                data={blogPost.author.avatar.responsiveImage}
+                className="blogPost-image"
+                fadeInDuration={1000}
+                data={blogPost.coverImage.responsiveImage}
               />
-              Written by {blogPost.author.name}
-            </footer>
-          </article>
-        ))}
+              <h6 className="blogPost-title">
+                <a
+                  href={`https://www.datocms.com/blog/${blogPost.slug}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {blogPost.title}
+                </a>
+              </h6>
+              <div
+                className="blogPost-excerpt"
+                dangerouslySetInnerHTML={{ __html: blogPost.excerpt }}
+              />
+              <footer className="blogPost-author">
+                <Image
+                  className="blogPost-author-image"
+                  data={blogPost.author.avatar.responsiveImage}
+                />
+                Written by {blogPost.author.name}
+              </footer>
+            </article>
+          ))}
       </div>
     </div>
   );

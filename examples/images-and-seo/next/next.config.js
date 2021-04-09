766999f6a4ccd9906720b088dfda6054e4c786a5
Stefano Verna <s.verna@datocms.com>
2021-04-09 14:43:16 +0200

    Upgrade examples dependencies

--- examples/images-and-seo/next/next.config.js
+++ examples/images-and-seo/next/next.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 module.exports = {
+  future: {
+    webpack5: true,
+  },
   webpack: (config) => {
     config.resolve.alias.react = path.join(__dirname, "node_modules/react");
     return config

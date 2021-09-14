f643729ed35ce384490b5b8c314364767bc34669
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2021-09-14 18:07:40 +0200

    Updating some packages

--- examples/images-and-seo/next/next.config.js
+++ examples/images-and-seo/next/next.config.js
@@ -1,11 +1,9 @@
 const path = require("path");
 
 module.exports = {
-  future: {
-    webpack5: true,
-  },
+  webpack5: true,
   webpack: (config) => {
     config.resolve.alias.react = path.join(__dirname, "node_modules/react");
-    return config
+    return config;
   },
-}
+};

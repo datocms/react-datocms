cff8d03b93cc8f868dfdcdc5cf2b77413a820de6
Silvano Stralla <silvano.stralla@sistrall.it>
2023-04-12 16:53:46 +0200

    Upgrade dependencies for Typescript example

--- examples/query-subscription/typescript/src/index.tsx
+++ examples/query-subscription/typescript/src/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./App";
 
-createRoot(
-  <App />,
+const domNode = document.getElementById('root');
+const root = createRoot(domNode!);
 
-  document.getElementById("root")
-);
+root.render(<App />);

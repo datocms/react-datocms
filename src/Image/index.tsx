88ca5ce7ce9679c30603f8ea587cbf165a693508
Stefano Verna <s.verna@datocms.com>
2023-04-27 15:48:17 +0200

    Wrap third-party component that rely on client-only features in a Client Component
    https://beta.nextjs.org/docs/rendering/server-and-client-components#third-party-packages

--- src/Image/index.tsx
+++ src/Image/index.tsx
@@ -6,7 +6,7 @@ import React, {
   useRef,
   useEffect,
 } from 'react';
-import { useInView } from 'react-intersection-observer';
+import { useInView } from './useInView';
 import { encode } from 'universal-base64';
 
 const isSsr = typeof window === 'undefined';
---
+++ src/Image/useInView.tsx
@@ -0,0 +1,3 @@
+'use client';
+
+export { useInView } from 'react-intersection-observer';

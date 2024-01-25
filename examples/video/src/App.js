2b3f30b860a37403f7079d7f90574b8d5e08a970
Silvano Stralla <silvano.stralla@sistrall.it>
2024-01-25 17:48:17 +0100

    Add an example with the  <VideoPlayer /> component

---
+++ examples/video/src/App.js
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+import './App.css';
+
+import { VideoPlayer } from 'react-datocms';
+
+const data = {
+  muxPlaybackId: 'ip028MAXF026dU900bKiyNDttjonw7A1dFY',
+  title: 'Title',
+  width: 1080,
+  height: 1920,
+};
+
+const blurUpThumb = "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHBwgHBgoICAgLDhAWDhYQDg0NDhUVFg0OFxUZGBYfFiEaHysjHR0oHRUWJDUlKC0vMjIyGSI4PTcwPCsxMi8BCgsLDg0OEA4QEC8dFhwvLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vLy8vL//AABEIABkADgMBIgACEQEDEQH/xAAYAAEAAwEAAAAAAAAAAAAAAAADBAUGAf/EABwQAAICAgMAAAAAAAAAAAAAAAEDAAIEEQUGIf/EABUBAQEAAAAAAAAAAAAAAAAAAAIB/8QAGBEAAgMAAAAAAAAAAAAAAAAAAAIBITH/2gAMAwEAAhEDEQA/AByuKdRJlXTiXWJkfI7bkOprUJPYnAHcSzQXizNl9teCCXuB8EWckUjaf//Z"
+
+function App() {
+  const ref = useRef()
+
+  return (
+    <div className="App">
+      <p>Video example</p>
+      <VideoPlayer data={data} className="video-player" ref={ref} placeholder={blurUpThumb}  />
+    </div>
+  );
+}
+
+export default App;

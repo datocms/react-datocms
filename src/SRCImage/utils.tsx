4b3d0f52d253da2df94352228317d7c27ed370c8
Stefano Verna <s.verna@datocms.com>
2024-06-12 15:43:12 +0200

    Fix warning with `fetchPriority` (closes #100)

--- src/SRCImage/utils.tsx
+++ src/SRCImage/utils.tsx
@@ -1,19 +1,16 @@
-import React, { version } from 'react';
+import React from 'react';
 import type { ResponsiveImageType } from '../Image';
 
 export function priorityProp(
   fetchPriority?: string,
 ): Record<string, string | undefined> {
-  const [majorStr, minorStr] = version.split('.');
-  const major = Number.parseInt(majorStr, 10);
-  const minor = Number.parseInt(minorStr, 10);
-
   // https://github.com/vercel/next.js/commit/68d5a3892b4edf1b62e3ece4ada324065a96b6f3
-  if (major > 18) {
+
+  // biome-ignore lint/suspicious/noExplicitAny: React.use is only available on React 19, react@canary and react@beta
+  if (typeof (React as any).use === 'function') {
     return { fetchPriority };
   }
-  // In React 18.2.0 or older, we must use lowercase prop
-  // to avoid "Warning: Invalid DOM property".
+
   return { fetchpriority: fetchPriority };
 }
 
